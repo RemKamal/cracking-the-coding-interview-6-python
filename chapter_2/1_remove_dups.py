46313d981ae6864b2de5f0b119cbaaab21ebb29d
Yong <youshking@gmail.com>
2015-11-05 21:23:58 -0500

    finish 2.1

--- chapter_2/1_remove_dups.py
+++ chapter_2/1_remove_dups.py
@@ -1,5 +1,35 @@
 import unittest
-from linked_list import random_list
+from linked_list import *
 
-ll = random_list()
-print ll
+def remove_dups(linked_list):
+    node = linked_list.head
+    tracer = [node.value]
+    pre = None
+    while node is not None:
+        pre = node
+        node = node.next
+        if node is not None:
+            if node.value in tracer:
+                pre.next = node.next
+                node = pre
+            else:
+                tracer.append(node.value)
+    return linked_list
+
+class Test(unittest.TestCase):
+
+    def unique_items(self, linked_list):
+        items = set()
+        for i in linked_list:
+            items.add(i)
+        return len(items)
+
+    def test(self):
+        ll1 = random_list(5, 1)
+        ll2 = random_list(100, 42)
+
+        self.failUnlessEqual(len(remove_dups(ll1)), self.unique_items(ll1))
+        self.failUnlessEqual(len(remove_dups(ll2)), self.unique_items(ll2))
+
+if __name__ == "__main__":
+    unittest.main()
--- chapter_2/linked_list.py
+++ chapter_2/linked_list.py
@@ -1,4 +1,4 @@
-from random import randint
+import random
 
 class LinkedList():
     """Helper function to create a linked list in Python"""
@@ -38,11 +38,14 @@ class LinkedList():
         node = self.head
         found = False
         previous = None
-        while not found:
+        while not found and node is not None:
             if node.value == item:
                 found = True
             else:
+                previous = node
                 node = node.next
+
+        if node is None: raise KeyError("item not in the list!")
         if previous is None:
             self.head = node.next
         else:
@@ -67,7 +70,13 @@ class LinkedList():
             result += str(item) + " -> " + str(linked) + "\n"
         return result
 
-
+    def __eq__(self, new_linked_list):
+        node1, node2 = self.head, new_linked_list.head
+        while node1 is not None and node2 is not None:
+            if node1.value != node2.value:
+                return False
+            node1, node2 = node1.next, node2.next
+        return True
         
 class Node():
     """Node object in LinkedList"""
@@ -84,9 +93,11 @@ class Node():
     def __str__(self):
         return str((self.value, self.next))
 
-def random_list():
-	ll = LinkedList()
-	for i in range(15):
-		ll.insert(i)
-		ll.insert(randint(0, 20))
-	return ll
+def random_list(n, random_state=42):
+    ll = LinkedList()
+    random.seed(random_state)
+    randoms = [random.randint(0, n * 2) for i in range(n)]
+    for i in range(n):
+        ll.insert(i)
+        ll.insert(randoms[i])
+    return ll
